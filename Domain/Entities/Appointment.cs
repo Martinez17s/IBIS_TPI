9448755113487430c6708c2cff06eb980e929538
Lorenzo Martinez <lmar190504@gmail.com>
2024-10-07 00:06:14 -0300

    Add entities, enums, and interface for Professional. Add entities for Appointment, Patient, and an abstract class User.

---
+++ Domain/Entities/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public Patient Patient { get; set; }
+        public Professional Professional { get; set; }
+    }
+}
